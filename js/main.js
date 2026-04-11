064693baf60acdae0aa50699bd1f76b731fa23f7
Veta <vetarara@yandex.ru>
2026-04-12 00:49:51 +0300

    добавляет  файл api с promise и async/await

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ import './post-viewer.js';
 import './form.js';
 
 createPosts();
+
+// посты загружаются
+// НАПРЯМУЮ FETCH-ЗАПРОС ПИСАТЬ НЕ НАДО
+fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json()) // получаем объект ответа. сервер отдаёт данные в формате json
+  .then((posts) => {
+    console.log(posts);
+  });
+
